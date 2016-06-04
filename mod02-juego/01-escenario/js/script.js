677cfb22a833dad812a63cd52b29279f70a3af5e
alumno1 <alumno1@A51MM-02.local>
2016-06-04 10:19:31 -0500

    Clase02

--- mod02-juego/01-escenario/js/script.js
+++ mod02-juego/01-escenario/js/script.js
@@ -1,27 +1,17 @@
 var JuegoEstado = {
-	// Aquí se inicializan las variables
 	init: function(){
 		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-		this.scale.pageAlignHorizontally = true;
 		this.scale.pageAlignVertically = true;
+		this.scale.pageAlignHorizontally = true;
 	},
-	// Aquí se cargan las imágenes, audios, etc.
 	preload: function(){
-		console.log("función preload");
+		
 	},
-	// Aquí se dibuja, se anima, etc.
-	create: function(){
-		console.log("función create");
-	},
-	// Esta función se ejecuta muchas veces
-	update: function(){
-		console.log("función update");
-	}
-}
-
+	create: function(){},
+	update: function(){}
+};
 
 var game = new Phaser.Game(722, 642, Phaser.AUTO);
-game.state.add("JuegoEstado", JuegoEstado);
 
-game.state.start("JuegoEstado");
-
+game.state.add("juegoEstado", JuegoEstado);
+game.state.start("juegoEstado");
