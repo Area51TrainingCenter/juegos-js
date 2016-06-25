444e3b1341ec9d73932835efb0d35b123094025b
alumno1 <alumno1@A51MM-02.local>
2016-06-25 11:52:22 -0500

    Clase05

---
+++ mod06-juego/04-objetos/js/estados/boot.js
@@ -0,0 +1,20 @@
+var Juego = Juego || {};
+
+Juego.BootEstado = {
+	init: function(){
+	    this.game.stage.backgroundColor = '#fff';  
+	    
+	    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+	    
+	    this.scale.pageAlignHorizontally = true;
+	    this.scale.pageAlignVertically = true;	
+	    
+    	this.game.physics.startSystem(Phaser.Physics.ARCADE);    	    	
+	},
+	preload: function(){
+		this.load.image('barra', 'img/preloader-bar.png');		
+	},
+	create: function(){
+		Juego.game.state.start('CargadorEstado');
+	}
+}
